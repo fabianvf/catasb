4b44728901f70dbf80c1c21b9aebcdd2a922e103
John Matthews <jwmatthews@gmail.com>
2017-05-21 11:45:47 -0400

    Updates from testing on Linux

--- local/linux/reset_environment.sh
+++ local/linux/reset_environment.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-export RESET_ENV="True"
 
-ANS_CODE="../ansible"
-source ./my_vars
+ANS_CODE="../../ansible"
 source ./common_vars
 ansible-playbook ${ANS_CODE}/reset_local_environment.yml --extra-vars "${EXTRA_VARS}"
--- local/linux/run_setup_local.sh
+++ local/linux/run_setup_local.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-export RESET_ENV="False"
 
-ANS_CODE="../ansible"
-source ./my_vars
+ANS_CODE="../../ansible"
 source ./common_vars
 ansible-playbook ${ANS_CODE}/setup_local_environment.yml --extra-vars "${EXTRA_VARS}"
