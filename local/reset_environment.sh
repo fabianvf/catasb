f8f98fdad8400651d02171820eeea8651723d707
Fabian von Feilitzsch <fabian@fabianism.us>
2017-05-25 10:40:25 -0400

    update reset_environmnent, add support for my_vars.yml

--- local/reset_environment.sh
+++ local/reset_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export RESET_ENV="True"
 
+extra_args=''
 ANS_CODE="../ansible"
 source ./common_vars
-[[ ! -e my_vars ]] || source ./my_vars
-ansible-playbook ${ANS_CODE}/reset_local_environment.yml --extra-vars "${EXTRA_VARS}" $@
+[[ ! -e my_vars.yml ]] || extra_args='-e @my_vars.yml'
+ansible-playbook ${ANS_CODE}/reset_local_environment.yml --extra-vars "${EXTRA_VARS}" $extra_args $@
